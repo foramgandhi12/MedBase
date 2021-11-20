17552bf5a94379234fb8dc288d3ca0b0827ed4c8
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2021-11-20 18:39:42 -0500

    Fixed errors in login

--- verifyLogin.php
+++ verifyLogin.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "med_base";
+$db = "medbase";
 
 // Create connection
 $database = mysqli_connect($servername, $username, $password, $db);
@@ -17,24 +17,34 @@ if (mysqli_connect_errno()) {
 //retrieve the values from the form
 else {    
     //check if the form is submitted
-    if (isset($_POST["submit"])){
+    if (isset($_POST['submit'])){
         //retrieve all values submitted within form
         $username_form = mysqli_real_escape_string($database, $_POST["name"]);
         $password_form = mysqli_real_escape_string($database, $_POST["password"]);
         $position_form = mysqli_real_escape_string($database, $_POST["position"]);
 
-        //perform query
-        $query = "SELECT * FROM employee WHERE employeeName = $username_form AND employeePassword = $password_form AND roleID = $position_form";
+        echo $username_form;
+        echo $password_form;
+        echo $position_form;
+
+        //perform query 
+        $query = "SELECT * FROM medbase.employee";
+
+        // $query = "SELECT * FROM medbase.employee WHERE employeeName = $username_form AND employeePassword = $password_form AND roleID = $position_form";
         $result = mysqli_query($database, $query);
-        //check to see if login was valid
-        if(!$result){
-            header("Location: Login.html");
-            $msg = "Invalid login! Please re-enter your credentials.";
-            echo '<script type="text/javascript">alert("' . $msg . '")</script>';
-        }
-        else{
-            header("Location: dashboard.php");
-        }
+
+        echo "\nThe result is ".mysqli_fetch_row($result);
+
+        // //check to see if login was valid
+        // if(!$result){
+        //     // header("Location: Login.html");
+        //     $msg = "Invalid login! Please re-enter your credentials.";
+        //     echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+        // }
+        // else{
+        //     echo "entered dashboard";
+        //     // header("Location: /dashboard.php");
+        // }
     }
 }
 ?>
