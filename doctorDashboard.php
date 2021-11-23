d15150b800efcff04915d9a05f2367fbaf75350a
Foram <foram4444@gmail.com>
2021-11-23 00:23:34 -0500

    fixed dashboard

---
+++ doctorDashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Doctor Dashboard</title>
+    </head>
+    <body>
+        <?php 
+            ob_start();
+            include 'dashboard.php';
+            $contents = ob_get_clean();
+            $doc = new DomDocument;
+            echo $contents;
+            libxml_use_internal_errors(true);
+            $doc->loadHTML($contents);
+
+            echo "The contents are ".$doc->getElementById('widget_row')->textContent;
+        ?>
+    </body>
+</html>
