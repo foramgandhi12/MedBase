8cb94ea722fa6d991768609b87c99bf537428770
Foram <foram4444@gmail.com>
2021-11-24 17:55:43 -0500

    created post surgery assessment module

---
+++ sendEmail.php
@@ -0,0 +1,48 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+include "employee.php";
+
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['subbtn'])){
+    
+    $email = $_POST['inputEmail'];
+    $surgery_type = $_POST['inputSurgery'];
+    $operation_procedure = $_POST['inputProcedure'];
+    $prescription = $_POST['inputPrescription'];
+    $postCare = $_POST['inputCare'];
+
+    $database = mysqli_connect('localhost', 'root', '', 'final_project');
+
+    // message
+    $msg = "<html>
+                <body>
+                    <br>
+                    Thank you for choosing our hospital! The following information has been conveyed to you by your surgeon regarding post-surgery care.<br><br>
+                    Surgery Type: <b>$surgery_type</b><br>
+                    Operation Procedure: <b>$operation_procedure</b><br>
+                    Prescription(s): <b>$prescription</b><br>
+                    Post-Surgery Care: <b>$postCare</b><br><br><br>
+                    Take Care!<br>
+                    <b>MedBase</b>
+                <body>
+            <html>";
+    
+    $message = wordwrap($msg, 150);
+
+    //formats html in the email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: medbase@gmail.com';
+    $subject = $_POST['inputSubject'];
+
+    if (isset($_POST['checkCopy'])) {
+        // sends email to both patient and employee
+        mail($email, $subject, $message, $headers);
+    }
+    else {
+        // sends email to only patient
+        mail($email, $subject, $message, $headers);
+    }
+}
+?>
