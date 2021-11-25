6294af7da3deeb8f44888a478ebf2b1add817d46
AryanS0301 <80480634+AryanS0301@users.noreply.github.com>
2021-11-25 02:18:30 -0500

    added google calendar

--- schedule.php
+++ schedule.php
@@ -5,6 +5,8 @@
         
         <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="public/css/schedule.css">
+        <script src="public/js/schedule.js"></script>
         
         <?php include "employee.php"; ?> 
         <?php include "setupDatabaseConnection.php"; ?>
@@ -14,7 +16,7 @@
         <?php echo $imgLink = add_sidebar($empRoleID ,$empName) ?>
 
         <div class = "content-wrapper">
-            test
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FToronto&src=bXM3ZnRyc2k0cGdrNGYxdTI5bTdocWl2azRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y2xhc3Nyb29tMTExNzI0MTY0ODA5ODQxNDY0NTg4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23F6BF26&color=%234a148c" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </div>
     </body>
 </html>
