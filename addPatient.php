c02a417bfa6cd53974ee51f625722d30a80e295d
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2021-11-24 19:20:23 -0500

    Backend for adding patient

--- addPatient.php
+++ addPatient.php
@@ -7,7 +7,6 @@ if ($database != null){
     $result= mysqli_query($database, $query);
     $index = intval(mysqli_fetch_row($result)[0]) + 1;
 
-    // print_r($_POST);
     // form fields
     $patientName = $_POST['patientName'];
     $patientAddress = $_POST['address'];
@@ -16,15 +15,15 @@ if ($database != null){
     $patientReasonForVisit = $_POST['reasonForVisit'];
     $patientWard = $_POST['ward'];
     $patientRoomId = $_POST['roomID'];
-    $patientCheckInDateTime = $_POST['checkInDateTime'];
+    $patientCheckInDateTime = str_replace("T", " ", $_POST['checkInDateTime']).":00";
     $patientDoctor = $_POST['doctor'];
     $patientNurse = $_POST['nurse'];
-    echo "<script>console.log('$patientCheckInDateTime');</script>";
 
-
-    // $insertPatientQuery = "INSERT INTO patients (patientID, patientName, patientAddress, patientEmail, patientPhoneNumber, ReasonForVisit, 
-    // ward_id, roomID, CheckInDate, doctorID, nurseIDm is_deceased) VALUES ('$index', '$patientName', '$patientAddress', '$patientEmail', 
-    // '$patientPhoneNumber', '$patientReasonForVisit', '$patientWard', '$patientRoomId', '$patientCheckInDateTime', '$patientDoctor', '$patientNurse', '0')";
-    // $insertPatientResult = mysqli_query($database, $insertPatientQuery);
+    // Script to insert patient with form fields
+    $insertPatientQuery = "INSERT INTO patients(patientID, patientName, patientAddress, patientEmail, patientPhoneNumber, ReasonForVisit, ward_ID, roomID, CheckInDate, doctorID, nurseID, is_deceased) VALUES ('$index', '$patientName', '$patientAddress', '$patientEmail', '$patientPhoneNumber', '$patientReasonForVisit', '$patientWard', '$patientRoomId', '$patientCheckInDateTime', '$patientDoctor', '$patientNurse', '0')"; 
+    // Run query
+    $insertPatientResult = mysqli_query($database, $insertPatientQuery);
+    // After execution return back to the patient registration page
+    header("Location: patientRegistration.php");
 }
 ?>
