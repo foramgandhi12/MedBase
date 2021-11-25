e214aa1db0a116bd9390b71ad650ef35246beb61
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2021-11-25 17:19:40 -0500

    changes

--- doc_list.php
+++ doc_list.php
@@ -2,8 +2,45 @@
 <html>
 <head>
     <title>Doctor List</title>
+    <style>
+        table{
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th{
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        tr: nth-child(even){
+            background-color: #f2f2f2;
+        }
+        
+        tr: hover{
+            background-color: #ddd;
+        }
+
+        th{
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #4c9baf;
+            color: black;
+        }
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 18px;
+        }
+        
+    </style>
 </head>
 <body>
+    <div>
+        <h1 style = "text-align: center;">List of Doctors</h1>
+    </div>
+
     <?php
         include "setupDatabaseConnection.php";
         $database = setupConnection();
@@ -12,13 +49,42 @@
             exit();
         }
         else{
+            $sqlQ = "SELECT * FROM employee
+            LEFT JOIN departments
+            ON employee.employeeDepartment = departments.departmentID
+            UNION
+            SELECT * FROM employee
+            RIGHT JOIN departments
+            ON employee.employeeDepartment = departments.departmentID";
+            $result = mysqli_query($database, $sqlQ);
+
+            echo "<table> <tr> <th>ID</th> <th>Name</th> <th>SSN</th> <th>Position</th> 
+            <th>Department</th> <th>Address</th> <th>Phone Number</th> <th>Password</th> </tr>";
+
+            while ($row = mysqli_fetch_row($result)){
+                echo "<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> 
+                <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td>".$row[6]."</td> 
+                <td>".$row[7]."</td> </tr>";
+            }
+            echo "</table>";
+        }
+        mysqli_close($database); 
+        
+        /*else{
             $sqlQ = "SELECT * FROM employee WHERE roleID = 1";
             $result = mysqli_query($database, $sqlQ);
+
+            echo "<table> <tr> <th>ID</th> <th>Name</th> <th>SSN</th> <th>Position</th> 
+            <th>Department</th> <th>Address</th> <th>Phone Number</th> <th>Password</th> </tr>";
+
             while ($row = mysqli_fetch_row($result)){
-                echo "<br> id: ". $row[0]. " - Name: ". $row[1]. "<br>";
+                echo "<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> 
+                <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td>".$row[6]."</td> 
+                <td>".$row[7]."</td> </tr>";
             }
+            echo "</table>";
         }
-        mysqli_close($database);
+        mysqli_close($database); */
     ?>
 
 
